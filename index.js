575d5b1d24af53e110104cc0a3ef6998a3af106d
Brad Schrag <brad.schrag@gmail.com>
2018-06-23 06:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,31 @@ function destructivelyAppendKitten(name) {
 }
 
 function destructivelyPrependKitten(name) {
-  kittens.
+  kittens.unshift(name)
+  return
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.unshift()
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0,-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1,)
 }
